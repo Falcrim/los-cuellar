3cbe30dfe84e05c5df9f3df4b9e3aa53511680a1
Angelo Fabio Cuellar Villalobos <villcuellar99@gmail.com>
2025-07-09 00:23:40 -0400

    actualizar ubicaciones
    Se añadio ubicacion en area para secciones (dibujar area en mapa)
    se vinculo recinto con seccion (un recinto pertenece a una ubicacion dentro de una Seccion)

--- Sistema_Administracion/administracion_electoral/models/recinto.py
+++ Sistema_Administracion/administracion_electoral/models/recinto.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+from administracion_electoral.models import Seccion
+
+
 class Recinto(models.Model):
     nombre = models.CharField(max_length=100)
     direccion = models.CharField(max_length=200)
     latitud = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
     longitud = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    seccion = models.ForeignKey(Seccion, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nombre
--- Sistema_Administracion/administracion_electoral/models/seccion.py
+++ Sistema_Administracion/administracion_electoral/models/seccion.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+
 class Seccion(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True, null=True)
+    area_geografica = models.JSONField(default=list, blank=True)
 
     def __str__(self):
         return self.nombre
