0392c0426948ee40a9db155af43b0d1653246f2c
Angelo Fabio Cuellar Villalobos <villcuellar99@gmail.com>
2025-07-11 00:01:28 -0400

    Update mesa_viewset
    Unicidad de jurados por mesa

--- Sistema_Administracion/administracion_electoral/apis/mesa_viewset.py
+++ Sistema_Administracion/administracion_electoral/apis/mesa_viewset.py
@@ -12,6 +12,41 @@ class MesaElectoralSerializer(serializers.ModelSerializer):
         model = MesaElectoral
         fields = ['id', 'numero', 'recinto', 'recinto_nombre', 'jurado_ids']
 
+    def validate_jurado_ids(self, value):
+        # Asegurar que el input sea siempre una lista
+        if isinstance(value, int):
+            value = [value]
+        elif not isinstance(value, list):
+            raise serializers.ValidationError("El campo 'jurado_ids' debe ser un entero o una lista de enteros.")
+
+        # Validar que todos los elementos sean enteros
+        if not all(isinstance(j, int) for j in value):
+            raise serializers.ValidationError("Todos los IDs de jurado deben ser enteros.")
+
+        current_instance_id = self.instance.id if self.instance else None
+
+        mesas = MesaElectoral.objects.all()
+        if current_instance_id:
+            mesas = mesas.exclude(id=current_instance_id)
+
+        jurados_usados = set()
+        for mesa in mesas:
+            jurado_ids = mesa.jurado_ids
+            # Forzamos que sea lista
+            if isinstance(jurado_ids, int):
+                jurado_ids = [jurado_ids]
+            elif not isinstance(jurado_ids, list):
+                jurado_ids = []
+            jurados_usados.update(jurado_ids)
+
+        for jurado_id in value:
+            if jurado_id in jurados_usados:
+                raise serializers.ValidationError(
+                    f"El jurado con ID {jurado_id} ya está asignado a otra mesa."
+                )
+
+        return value
+
 
 class MesaElectoralViewSet(viewsets.ModelViewSet):
     queryset = MesaElectoral.objects.all()
